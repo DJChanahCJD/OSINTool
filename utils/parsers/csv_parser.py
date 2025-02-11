2d0406d11d4e5b22c22568332b2b1b57edf68b14
浪小矢 <djchan411130@gmail.com>
2025-02-11 20:43:06 +0800

    fix: 修复csv空值问题

--- utils/parsers/csv_parser.py
+++ utils/parsers/csv_parser.py
@@ -9,9 +9,11 @@ class CSVParser(BaseParser):
         self.load_content(url)
         # 使用 pandas 读取 CSV 内容
         df = pd.read_csv(StringIO(self.content))
+
+        # 将 DataFrame 转换为二维数组，并处理空值
+        table = df.fillna('').values.tolist()
+
         # 将标题行作为注释内容
         comment = df.columns.tolist()
-        # 将 DataFrame 转换为二维数组
-        table = df.values.tolist()
 
         return table, comment
