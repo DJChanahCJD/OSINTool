268664947fdfb9c0c9ba8dcaecc0f0e488f5e1ba
浪小矢 <djchan411130@gmail.com>
2025-02-18 18:23:56 +0800

    fix: 修复一些bug，暂时放弃导入导出功能

--- frontend/main.js
+++ frontend/main.js
@@ -38,62 +38,12 @@ function createWindow() {
             // 发送消息给渲染进程
             mainWindow.webContents.send('quit');
         }
-
-        // 如果你想阻止窗口关闭，可以使用 e.preventDefault()
-        // e.preventDefault();
     });
 
 }
 
-function createEditWindow(mainWindow) {
-    editWindow = new BrowserWindow({
-      width: 600,
-      height: 400,
-      parent: mainWindow, // 让 editWindow 作为 mainWindow 的子窗口
-      modal: true, // 使用模态窗口，确保不能在 edit.html 上与其他窗口交互
-      webPreferences: {
-        nodeIntegration: true
-      }
-    });
-
-    editWindow.loadFile('edit.html');
-  }
-
 app.whenReady().then(() => {
-    createWindow();
-
-    // 获取当前菜单，如果没有设置菜单，会返回 null
-    let menu = Menu.getApplicationMenu();
-    if (!menu) {
-        // 如果没有菜单，创建一个空的菜单模板
-        menu = Menu.buildFromTemplate([]);
-    }
-
-    // 在原菜单基础上添加新的菜单项
-    const newMenuItem = {
-        label: '自定义菜单',
-        submenu: [
-            {
-                label: '子菜单项 1',
-                click: () => {
-                    console.log('点击了子菜单项 1');
-                }
-            },
-            {
-                label: '子菜单项 2',
-                click: () => {
-                    console.log('点击了子菜单项 2');
-                }
-            }
-        ]
-    };
-
-    // 将新的菜单项添加到菜单中
-    menu.append(new MenuItem(newMenuItem));
-
-    // 重新设置应用程序菜单
-    Menu.setApplicationMenu(menu);
-
+    mainWindow = createWindow();
     app.on('activate', function () {
         if (BrowserWindow.getAllWindows().length === 0) createWindow();
     });
@@ -104,6 +54,3 @@ app.on('window-all-closed', function () {
         app.quit();
     }
 });
-
-// 引入 MenuItem 类
-const { MenuItem } = require('electron');
